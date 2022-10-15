c5835f47ced2f0bd0bbbe9e0f1034be11d205efc
Suchimauzic <113298056+Suchimauzic@users.noreply.github.com>
2022-10-15 10:23:04 +0300

    Delete task11_Alex_Gus.py

--- task11_Alex_Gus.py
+++
@@ -1,8 +0,0 @@
-def triangle(a, b, c):
-    if a + b > c and a + c > b and b + c > a:
-        return 'Треугольник по заданным сторонам существует'
-    else:
-        return 'Треугольника по заданным сторонам не существует'
-
-first, second, third = int(input('Введите первую сторону треугольника: ')), int(input('Введите вторую сторону треугольника: ')), int(input('Введите третью сторону треугольника: '))
-print(triangle(first, second, third))
