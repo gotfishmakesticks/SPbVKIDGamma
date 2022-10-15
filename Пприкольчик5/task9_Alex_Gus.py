58fe90a3cb4894f1035620435a7b2e5c8e7b36bb
Suchimauzic <113298056+Suchimauzic@users.noreply.github.com>
2022-10-15 10:30:19 +0300

    Update task9_Alex_Gus.py

--- Пприкольчик5/task9_Alex_Gus.py
+++ Пприкольчик5/task9_Alex_Gus.py
@@ -1,4 +1,4 @@
 weight = int(input("Введите вес: "))
-high = int(input("Введите рост: "))
-total = weight / high
+high = float(input("Введите рост: "))
+total = weight / (high ** 2)
 print("Индекс массы тела: " + str(total))
