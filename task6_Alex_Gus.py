ced0abd3eeb407b27b7e9079116527fa6e04f869
Suchimauzic <113298056+Suchimauzic@users.noreply.github.com>
2022-10-15 10:31:12 +0300

    Delete task6_Alex_Gus.py

--- task6_Alex_Gus.py
+++
@@ -1,6 +0,0 @@
-print("Введите два числа")
-first, second, mult = int(input("Введите первый множитель: ")), int(input("Введите второй множитель: ")), 0
-
-for i in range(second):
-    mult += first
-print('Произведение равно: '+ str(mult))
